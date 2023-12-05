b54d932b10cac846be8a8c12cef248a8a396ccc1
spayba <seanpayba@gmail.com>
2023-12-05 00:05:27 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+import configRoutes from './routes/index.js';
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
